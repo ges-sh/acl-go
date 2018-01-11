0248b7ef713e00faf7b6b82190c69dd57a1294bb
Łukasz Opasiak <lo@idesign.center>
2018-01-11 15:24:10 +0100

    Remove perms function

--- acl.go
+++ acl.go
@@ -2,7 +2,6 @@ package acl
 
 type ACL interface {
 	Can(int, int, uint64) bool
-	Perms(int, int) uint64
 }
 
 type Acl map[int]map[int]uint64
@@ -30,8 +29,4 @@ func (a Acl) AddRole(r int, inh ...int) {
 
 func (a Acl) Can(r int, o int, p uint64) bool {
 	return a[r][o]&p != 0
-}
-
-func (a Acl) Perms(r int, o int) uint64 {
-	return a[r][o]
 }
